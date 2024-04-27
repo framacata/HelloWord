330a86b9225e4361a781624236bbea7021a6512a
framacata <167640435+framacata@users.noreply.github.com>
2024-04-27 12:34:23 -0600

    Update MF24026.php

--- MF24026.php
+++ MF24026.php
@@ -1,8 +1,6 @@
 <?php
 
-  $saludo = "Hola Mundo, mi carnet es MF24026: ";
-
-  $saludo = "Hola Mundo, mi carnet es MF24026, desde la rama develop";
+  $saludo = "Hola Mundo, mi carnet es MF24026, desde la rama release";
 
   echo $saludo;
 ?>
